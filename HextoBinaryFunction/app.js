99705c12fb71b677d96012e95f81a0dcf68cf89d
User <Willspri@gmail.com>
2024-01-22 16:10:14 +1300

    added code to handle other hex formats

--- HextoBinaryFunction/app.js
+++ HextoBinaryFunction/app.js
@@ -2,7 +2,9 @@
 
 //function to receive hex numbers
 function hex2bin(hex) {
+    //handle case where hex is entered using 0x format
+    const hexString = hex.startsWith("0x") ? hex.slice(2) : hex;
     return parseInt(hex, 16).toString(2).padStart(8, '0');
 }
 
-console.log(hex2bin("AA"));
+console.log(hex2bin("0xAA"));
